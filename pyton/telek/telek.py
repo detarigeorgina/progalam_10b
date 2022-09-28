f0eb4cba826fd92267fe8935817338f5b6974be8
detarigeorgina <detari.georgina@ckik.hu>
2022-09-28 10:34:25 +0200

    telek

---
+++ pyton/telek/telek.py
@@ -0,0 +1,12 @@
+#Konstansok
+hossz = 45
+szelesseg =35
+kapu = 5
+
+#Szamolas
+kerites = 2*(hossz + szelesseg) - Kapu
+alapterulet = hossz*szelesseg
+
+#Kiirás
+print ("A kerites hossza:", kerites, "meter.")
+print("A telek alapterulete:", alapterulet, "nm.")
