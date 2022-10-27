a9e58fe4445a48a091ed0a2fbf68385b8b5dd082
detarigeorgina <detari.georgina@ckik.hu>
2022-10-27 11:06:31 +0200

    ora

---
+++ ciklusok/primszam2 copy.py
@@ -0,0 +1,21 @@
+
+
+#2-től m-ig mnden szamrol eldöntjük, hogy prim-e
+m = int(input("m: "))
+print(2, end=" ")
+
+j = 3
+while j <= m:
+
+    n = j 
+    primE = True
+
+i = 2 
+while i < n and primE:
+    if n % i == 0:
+        primE == False
+    i+= 1
+
+if primE:
+    print(j, end=" ")
+
