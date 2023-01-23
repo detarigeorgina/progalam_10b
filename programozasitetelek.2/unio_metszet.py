8706c80ae8889bea6c0ce1e7acd95be4a16c0e1f
detarigeorgina <detari.georgina@ckik.hu>
2023-01-23 09:36:35 +0100

    ora

--- programozasitetelek.2/unio_metszet.py
+++ programozasitetelek.2/unio_metszet.py
@@ -1,2 +1,24 @@
+def metszet_kepzes(a, b):
+    m = []
+    for i in range(len(a)):
+        if a[i] in b:
+            m.apppend(a[i])
+    return m
+
+def unio_kepzes(a, b):
+    u = []
+    for i in range(len(a)):
+        u.append(a[i])
+
+    for i in range (len(b)):
+        if b[i] not in a:
+            u.append(b[i])
+    return u 
+
+
 matekosok = ["Anna", "Béla", "Csaba", "Dóra", "Erik", "Ferenc"]
 progosok = ["Ferenc", "József", "Dóra", "Anna", "Csaba"]
+metszet = metszet_kepzes(matekosok, progosok)
+print(metszet)
+
+unio = unio_kepzes(matekosok, progosok)
